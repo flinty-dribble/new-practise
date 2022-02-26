4c499b117099d1a0a15aae18927b92f73b4f2c50
Mirab <you@example.com>
2022-02-26 20:31:56 +0300

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
+};
