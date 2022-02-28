acbe55c789229a9a0833c66b411763468f38491b
Mirab <you@example.com>
2022-02-28 19:15:04 +0300

    homework 1 task 1

---
+++ src/homeworkN1/mul.test.js
@@ -0,0 +1,7 @@
+import { mul } from "./mul";
+
+describe("mul", () => {
+  it("mul of nums", () => {
+    expect(mul(4, 2)).toEqual(8);
+  });
+});
--- src/sum.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from "./sum";
-
-describe("sum", () => {
-  it("sum of num", () => {
-    expect(sum(1, 2)).toEqual(3);
-  });
-});
