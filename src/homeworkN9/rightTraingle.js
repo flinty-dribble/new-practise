2a014c2e883546e1f9bc615c1819c26300ae2208
Mirab <you@example.com>
2022-03-07 17:39:45 +0300

    homework 9 task 1

---
+++ src/homeworkN9/rightTraingle.js
@@ -0,0 +1,15 @@
+export function func(s1, s2, s3) {
+  const s11 = s1 * s1;
+  const s22 = s2 * s1;
+  const s33 = s3 * s3;
+  if (s11 === s22 + s33) {
+    return "rectangular";
+  }
+  if (s22 === s11 + s33) {
+    return "rectangular";
+  }
+  if (s33 === s11 + s22) {
+    return "rectangular";
+  }
+  return "not rectangular";
+}
